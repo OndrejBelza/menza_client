2b3ee4bee3eee107b42483cea25bc2472b119ee8
Ondřej Belza <belzaondra@gmail.com>
2022-07-17 10:40:37 +0200

    fix: storybook fix

--- src/components/blocks/RestaurantCard/RestaurantCard.stories.tsx
+++ src/components/blocks/RestaurantCard/RestaurantCard.stories.tsx
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { ComponentMeta, Story } from "@storybook/react";
 import RestaurantCard, { RestaurantCardProps } from "./RestaurantCard";
 
@@ -11,3 +12,12 @@ const Template: Story<RestaurantCardProps> = (args) => (
 );
 
 export const basic = Template.bind({});
+
+basic.args = {
+  id: faker.datatype.uuid(),
+  name: faker.lorem.words(3),
+  address: faker.address.streetAddress(),
+  openingHours: faker.lorem.words(5),
+  img: faker.image.animals(),
+  generateDetailLink: (id: string) => id,
+};
